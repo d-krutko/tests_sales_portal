4b5d7c1ac9f2fc6451687d209fcef53dbd3fc1c7
Dina Krutko <dinakrutko@ip-10-7-97-148.eu-west-1.compute.internal>
2025-10-31 11:00:09 +0300

    Page Object класс для страницы Sign In, e2e тест по добавлению продукта

--- src/ui/pages/products/productsList.page.ts
+++ src/ui/pages/products/productsList.page.ts
@@ -1,12 +1,30 @@
+import { MANUFACTURERS } from "data/salesPortal/products/manufacturers";
 import { SalesPortalPage } from "../salesPortal.page";
+import { IProductInTable } from "data/types/product.types";
 
 export class ProductsListPage extends SalesPortalPage {
   readonly productsPageTitle = this.page.locator("h2.fw-bold");
   readonly addNewProductButton = this.page.locator('[name="add-button"]');
   readonly tableRowByName = (productName: string) =>
-    this.page.locator("table tbody tr", { has: this.page.locator("td", { hasText: productName }) });
+    this.page.locator("table tbody tr", {
+      has: this.page.locator("td", { hasText: productName }),
+    });
 
   readonly uniqueElement = this.addNewProductButton;
+
+  async getLastProductData(productName: string): Promise<IProductInTable> {
+    const [name, price, manufacturer, createdOn] = await this.tableRowByName(
+      productName
+    )
+      .locator("td")
+      .allInnerTexts();
+    return {
+      name: name!,
+      price: +price!.replace("$", ""),
+      manufacturer: manufacturer! as MANUFACTURERS,
+      createdOn: createdOn!,
+    };
+  }
 
   async clickAddNewProduct() {
     await this.addNewProductButton.click();
